a6d45e60c3a019b3add55fc4f1ef6d704147fe0b
ScarletLeaf1000 <ScarletLeaf1000@gmail.com>
2025-10-24 14:47:09 -0700

    Initial Commit v3

--- src/main/java/me/scarletleaf1000/tempered/Config.java
+++ src/main/java/me/scarletleaf1000/tempered/Config.java
@@ -1,6 +1,10 @@
 package me.scarletleaf1000.tempered;
 
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
@@ -8,4 +12,14 @@ import net.neoforged.fml.common.EventBusSubscriber;
 public class Config
 {
 
+    private static boolean validateItemName(final Object obj)
+    {
+        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
+    }
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+
+    }
 }
